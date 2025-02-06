76973be411e7d86ca88bed72883a67b94c480929
AmanShukla00 <aman17101998@gmail.com>
2025-02-06 12:48:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 function firstWord(s) {
-  // your code here
-	  if (s ==" ") {
-    return;
+  if (s == "") {
+    return "";
   }
-  
   const spaceIndex = s.indexOf(' ');
-  
   if (spaceIndex == -1) {
     return s;
   }
-  
   return s.substring(0, spaceIndex);
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
